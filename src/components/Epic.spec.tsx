e639f861a16fc6bf143c00e0e64ea2e57a70d726
Jonah Dela Cruz <jonah@srcclr.com>
2020-03-16 19:26:27 +0800

    Fix build

---
+++ src/components/Epic.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Epic from 'components/Epic';
+
+describe('<Epic />', () => {
+  it('test', () => {
+    //hello world
+    expect(true);
+  });
+});
