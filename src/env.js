1af42ca9ab5c1e16a62aa6559490103dc95a644d
Jonah Dela Cruz <jonah@srcclr.com>
2020-06-24 18:45:25 +0800

    Change URL

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 window.SRCCLR_ENV = {
-  API_URL: 'https://pi-planning-be.ksp.ops2.srcclr.io', //'https://sapling-planning.herokuapp.com', //'http://localhost:8080',
+  API_URL: 'https://sapling-planning.herokuapp.com', //'http://localhost:8080',
   BOARD_REFRESH_RATE_MS: 5000,
 };
